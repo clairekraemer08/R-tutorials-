03f9e4066b17ac944dd224c3c182f33302ea44b9
clairekraemer08 <89663154+clairekraemer08@users.noreply.github.com>
2021-10-21 10:49:21 +0200

    update 1

--- spatial_analysis_r.R
+++ spatial_analysis_r.R
@@ -16,7 +16,14 @@ my_package <- c("raster", "OpenStreetMap",
 lapply(my_package, require, character.only = TRUE)
 
 #3.2 Intro to sp and sf from "GisTools" package 
-#sp --> New Haven and Georgia data 
+#sp data format  
+# FORMAT 
+# -------------------------------------
+#SpatialPoints | SpatialPointsDataFrame
+#SpatiaLines | SpatiaLinesDataFrame
+#SpatialPolygons | SpatialPolygonsDataFrame 
+#--------------------------------------
+# --> New Haven and Georgia data 
 data(newhaven) #load Data Haven
 ls()
 class(breach) #--> SpatialPoints
@@ -24,3 +31,18 @@ class(blocks) #--> SpatialPolygonsDataFrame
 head(data.frame(blocks)) #acess data from blocks
 head(blocks@data)
 plot(blocks) # plot the data  
+#plo several layers
+par(mar=c(0,0,0,0)) #set parameters to display the plot, mar defines the white space c(below, left,abive, right)
+plot(roads, col="red") #plot roads in red
+plot(blocks, add = T) # add blocks in black
+
+#sf data format "simple features" : seek to encode to conform formal standard (ISO 19125-1:2004)
+# Point , Linestring, Polygon, Multipoint, Multilinestring, Multipolygon
+vignette(package = "sf")
+vignette("sf1" ,package = "sf") #get detailed help 
+#sp object can be convert to sf
+data(georgia) #load data 
+georgia_sf = st_as_sf(georgia) #convert to sf 
+class(georgia_sf)
+head(data.frame(georgia))
+head(data.frame(georgia_sf)) #st dataframe havegeometry attribut in addition
